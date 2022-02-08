ae07e2614ccd2d5a93f10254212c4e77004b8edf
TheLichKingZeyd <NoMoreOppress@gmail.com>
2022-02-08 15:24:19 +0100

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+
+	fmt.Println("Hello()\a", quote.Hello())
+
+	fmt.Println("Go()\a", quote.Go())
+
+	fmt.Println("Glass()\a", quote.Glass())
+
+	fmt.Println("Opt()\a", quote.Opt())
+
+}
